e273c4bd8c5bf3abe3a20a8c6ef662a0c06266ea
GP <ganeshpaih24@gmail.com>
2022-06-23 00:44:25 +0530

    Added validation conditon

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def login_home(request):
@@ -9,5 +10,12 @@ def login_about(request):
     return render(request, 'user/about.html')
 
 def register(request):
-    form=UserCreationForm()
+    if request.method=='POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}:')
+            return redirect('user-login-home')
+    else
+        form=UserCreationForm()
     return render(request,'user/register.html',{'form':form}})
