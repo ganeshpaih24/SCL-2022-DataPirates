f4f9d33fa3dc436f54685a4a1c5e9fe11ce87093
SujathaBhat <sujathabhat6@gmail.com>
2022-06-25 19:40:12 +0530

    initial user model and view

--- user/views.py
+++ user/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 
 
 def login_home(request):
+    data=Post.objects.all()
+    context={
+        'posts':Post.objects.all()
+    }  
+    for i in data:
+        print(i)
+    
     return render(request, 'user/home.html')
 
 def login_about(request):
