0763e8ae530e1adad81c5920832f76a368aa16da
Prateek-Nayak <nprateek24@gmail.com>
2022-06-26 00:47:55 +0530

    Edited navbar

--- walkthrough_scl/urls.py
+++ walkthrough_scl/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from walkthrough_scl import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
