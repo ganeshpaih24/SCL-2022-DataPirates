a1bea6aa2bc87bb31d1e607525e99e77bc78d1bb
Prateek-Nayak <nprateek24@gmail.com>
2022-06-26 00:34:19 +0530

    Added template/includes

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def login_home(request):
-    return render(request, 'user/home.html')
+    return render(request, 'base.html')
 
 def login_about(request):
-    return render(request, 'user/about.html')
+    return render(request, 'base.html')
