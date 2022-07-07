dc67e7ad2bd417fa2ee7f25deef56bc0ab97f334
SujathaBhat <sujathabhat6@gmail.com>
2022-07-07 20:04:23 +0530

    profile and profile picture

--- user/models.py
+++ user/models.py
@@ -1,8 +1,10 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
@@ -12,3 +14,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+     
