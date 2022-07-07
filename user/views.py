eb7c3a8c5b442ef04ac89413c01d186fb23de387
GP <ganeshpaih24@gmail.com>
2022-07-08 00:12:12 +0530

    Added PostUpdateView

--- user/views.py
+++ user/views.py
@@ -1,10 +1,11 @@
 from .models import Post
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 
 from django.shortcuts import render,redirect
@@ -57,3 +58,17 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
