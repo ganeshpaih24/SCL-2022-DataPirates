a93211967f0a4db9fe26c8d06555d152553d4d95
GP <ganeshpaih24@gmail.com>
2022-07-22 20:39:43 +0530

    branch update

--- user/urls.py
+++ user/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteview
-)
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='user-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('', views.login_home, name='user-login-home'),
     path('about/', views.login_about, name='user-login-about'),
+    path('register/', views.register, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,5 @@
 from .models import Post
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
 
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
@@ -27,6 +19,7 @@ def login_about(request):
     return render(request, 'user/about.html')
 
 
+#to be added after register view
 @login_required
 def profile(request):
     return render(request,"user/profile.html")
@@ -42,44 +35,3 @@ def register(request):
     else:
         form=UserRegisterForm()
     return render(request,'user/register.html',{'form':form})
-
-class PostListView(ListView):
-    model=Post
-    template_name='user/home.html'
-    context_object_name='posts'
-    ordering=['date_posted']
-
-class PostDetailView(DetailView):
-    model=Post
-
-class PostCreateView(LoginRequiredMixin,CreateView):
-    model=Post
-    fields=['title','content']
-
-    def form_valid(self,form):
-        form.instance.author=self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
-    model=Post
-    fields=['title','content']
-
-    def form_valid(self,form):
-        form.instance.author=self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post=self.get_object()
-        if self.request.user==post.author:
-            return True
-        return False
-
-class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
-    model=Post
-    success_url='/'
-
-    def test_func(self):
-        post=self.get_object()
-        if self.request.user==post.author:
-            return True
-        return False
