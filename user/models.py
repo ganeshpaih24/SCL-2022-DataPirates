c33c6e2395ee031aaab9ffbe8e2a11a045c00c63
PriyankaKudva <priyankakudva06@gmail.com>
2022-07-23 18:28:00 +0530

    updated user profile

--- user/models.py
+++ user/models.py
@@ -1,25 +1,21 @@
-from email.policy import default
 from django.db import models
-
-from django.utils import timezone
 from django.contrib.auth.models import User
-
-
-class Post(models.Model):
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+from PIL import Image
 
 
 class Profile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
-    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-     
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
