b848600040218a9c1b3da2bfd0ef2790162ede7d
PriyankaKudva <priyankakudva06@gmail.com>
2022-07-23 17:56:41 +0530

    Update user profile

--- user/views.py
+++ user/views.py
@@ -6,8 +6,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
 
-def login_home(request):
-    return render(request, 'base.html')
 
 def login_about(request):
     context={
@@ -15,23 +13,10 @@ def login_about(request):
     }  
     return render(request, 'base.html')
 
-def login_about(request):
-    return render(request, 'user/about.html')
-
-
-#to be added after register view
 @login_required
 def profile(request):
     return render(request,"user/profile.html")
 
-def register(request):
-    if request.method=='POST':
-        form= UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}:')
-            return redirect('user-login-home')
     else:
         form=UserRegisterForm()
     return render(request,'user/register.html',{'form':form})
