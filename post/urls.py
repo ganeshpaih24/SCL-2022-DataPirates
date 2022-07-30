0fa9b10d2365d9d82b1d6503f92fe60ff5f4ee65
GP <ganeshpaih24@gmail.com>
2022-07-31 02:56:41 +0530

    Added Subpost features

--- post/urls.py
+++ post/urls.py
@@ -14,5 +14,9 @@ urlpatterns = [
     path('new/', PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    
+
+    path('subpost/',views.subpost,name="post-subpost"),
+    path('subpost/new/',views.createSubpost,name="subpost-create"),
+    path('subpost/update/<str:pk>/',views.updateSubpost,name="subpost-update"),
+    path('subpost/delete/<str:pk>',views.deleteSubpost,name="subpost-delete"),
 ]
