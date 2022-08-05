8df930b379a13e91c447110247c8d9b09fc7e877
GP <ganeshpaih24@gmail.com>
2022-08-05 16:19:48 +0530

    minor bug fixes

--- post/urls.py
+++ post/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('flow/', views.flowchart, name='flowchart'),
     #path('subpost/',views.subpost,name="post-subpost"),
     path('post/<int:pk>/subpost/new/',SubPostCreateView.as_view(),name="subpost-create"),
-    path('post/<int:id>/subpost/<int:pk>/update/',views.updateSubpost,name="subpost-update"),
-    path('post/<int:id>/subpost/<int:pk>/delete/',views.deleteSubpost,name="subpost-delete"),
+    path('post/<int:pk>/subpost/<int:id>/update/',views.updateSubpost,name="subpost-update"),
+    path('post/<int:pk>/subpost/<int:id>/delete/',views.deleteSubpost,name="subpost-delete"),
     path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
 ]
