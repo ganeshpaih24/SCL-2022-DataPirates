b15d30d73d637838cf4c7002c74f16083bf0e0bb
Prateek-Nayak <nprateek24@gmail.com>
2022-08-10 22:27:26 +0530

    None

--- post/urls.py
+++ post/urls.py
@@ -3,9 +3,9 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    #PostUpdateView,
+    # PostUpdateView,
     PostDeleteView,
-    #CommentCreateView,
+    # CommentCreateView,
     SubPostCreateView,
 )
 from . import views
@@ -17,11 +17,14 @@ urlpatterns = [
     path('update/<int:pk>/', views.postUpdateView, name='post-update'),
     path('delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     # path('flow/', views.flowchart, name='flowchart'),
-    #path('subpost/',views.subpost,name="post-subpost"),
+    # path('subpost/',views.subpost,name="post-subpost"),
 
-    path('post/<int:pk>/subpost/new/',SubPostCreateView.as_view(),name="subpost-create"),
-    path('post/<int:pk>/subpost/<int:id>/update/',views.updateSubpost,name="subpost-update"),
-    path('post/<int:pk>/subpost/<int:id>/delete/',views.deleteSubpost,name="subpost-delete"),
+    path('post/<int:pk>/subpost/new/',
+         SubPostCreateView.as_view(), name="subpost-create"),
+    path('post/<int:pk>/subpost/<int:id>/update/',
+         views.updateSubpost, name="subpost-update"),
+    path('post/<int:pk>/subpost/<int:id>/delete/',
+         views.deleteSubpost, name="subpost-delete"),
     #path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
 
 ]
