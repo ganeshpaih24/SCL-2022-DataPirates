5c937d1be833d7f731a505e4aa4c6ae5279cd2dd
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2022-08-17 20:10:39 +0530

    post and subpost layout

--- post/forms.py
+++ post/forms.py
@@ -32,7 +32,9 @@ class PostForm(forms.ModelForm):
             "category",
         )
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'image': forms.ImageField(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Title for the post',  'class': 'form-control', }),
+            # 'image':
+            'content': forms.Textarea(attrs={'placeholder': 'Title for the post', 'class': 'form-control'}),
+            # 'content': forms.CharField(),
+            # 'category': forms.Textarea(attrs={'placeholder': 'Title for the post', 'class': 'form-control'}),
         }
