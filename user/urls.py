7840e2c5e215b0120626962c7089800104e1187e
GP <ganeshpaih24@gmail.com>
2022-08-24 17:52:08 +0530

    minor bug fixes

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.login_about, name='user-login-about'),
+    path('', views.login_about, name='user-login-about'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 
