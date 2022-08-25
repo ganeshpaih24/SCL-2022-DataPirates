b249f715a4f00731cc8a1994ad3ab67873547844
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2022-08-26 01:42:43 +0530

    ready to deploy

--- walkthrough_scl/settings.py
+++ walkthrough_scl/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
