0b715f06844a2fc49f7807c545456b0caf22ab08
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2022-08-26 00:33:04 +0530

    ready to deploy

--- walkthrough_scl/settings.py
+++ walkthrough_scl/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
